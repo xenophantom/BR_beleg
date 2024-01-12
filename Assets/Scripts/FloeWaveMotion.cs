33aca03b9a46e93e93a3c4a965addf2eaee0a447
xenophantom <72735062+xenophantom@users.noreply.github.com>
2024-01-12 12:04:54 +0100

    player float movement added

---
+++ Assets/Scripts/FloeWaveMotion.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloeWaveMotion : MonoBehaviour
+{
+    float originalY;
+    public float floatStrength = 1;
+
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            originalY + ((float)System.Math.Sin(Time.time) * floatStrength),
+            transform.position.z);
+    }
+}
